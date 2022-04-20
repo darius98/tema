f293b9adf749fd0be30e0f3de7cf1bbce89078fb
Darius Marian <marian.darius98@gmail.com>
2022-04-19 21:31:39 -0400

    Better script for running tests.

--- check.sh
+++ check.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 
-cmake --build .build/debug --target unittests
+set -e
+
+echo "\nConfigure & build project\n"
+mkdir -p .build
+cmake -DCMAKE_BUILD_TYPE=Debug -B.build/debug -H.
+cmake --build .build/debug -j8
+
+echo "\nRun tests\n"
 LLVM_PROFILE_FILE=".build/unittests.profraw" ./.build/debug/tests/unittests --executor=smooth
 llvm-profdata merge -sparse .build/unittests.profraw -o .build/unittests.profdata
 llvm-cov show ./.build/debug/tests/unittests -instr-profile=.build/unittests.profdata \
@@ -8,5 +15,7 @@ llvm-cov show ./.build/debug/tests/unittests -instr-profile=.build/unittests.pro
   -show-line-counts \
   -use-color -Xdemangler c++filt -Xdemangler -n \
   -format=html > .build/coverage.html
+
+echo "\nLLVM Coverage Report:\n"
 llvm-cov report ./.build/debug/tests/unittests -instr-profile=.build/unittests.profdata
 open .build/coverage.html
