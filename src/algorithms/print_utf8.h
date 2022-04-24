28cc16c7f22fe5070f23baf3c7223980a56ee19a
Darius Marian <marian.darius98@gmail.com>
2022-04-24 15:09:00 -0400

    Fix typo

--- src/algorithms/print_utf8.h
+++ src/algorithms/print_utf8.h
@@ -7,7 +7,7 @@
 namespace tema {
 
 void print_utf8_to(const expression* expr, std::ostream& to);
-[[nodiscard]] std::string print_utf8(const expression* statement);
+[[nodiscard]] std::string print_utf8(const expression* expr);
 
 void print_utf8_to(const statement* statement, std::ostream& to);
 [[nodiscard]] std::string print_utf8(const statement* statement);
