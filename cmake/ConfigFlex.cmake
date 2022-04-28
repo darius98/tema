c1dbc0a7153699bd1326ffe89c94c6d229a22c06
Darius Marian <marian.darius98@gmail.com>
2022-04-28 17:36:22 -0400

    Fix CI build on Mac OS (attempt 2.0).

---
+++ cmake/ConfigFlex.cmake
@@ -0,0 +1,18 @@
+# On macOS, search Homebrew for keg-only versions of Flex. Xcode does
+# not provide new enough versions for us to use.
+if (CMAKE_HOST_SYSTEM_NAME MATCHES "Darwin")
+    execute_process(
+        COMMAND brew --prefix flex
+        RESULT_VARIABLE BREW_FLEX
+        OUTPUT_VARIABLE BREW_FLEX_PREFIX
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    if (BREW_FLEX EQUAL 0 AND EXISTS "${BREW_FLEX_PREFIX}")
+        message(STATUS "Found Flex keg installed by Homebrew at ${BREW_FLEX_PREFIX}")
+        set(FLEX_EXECUTABLE "${BREW_FLEX_PREFIX}/bin/flex")
+        include_directories(${BREW_FLEX_PREFIX}/include)
+        link_directories(${BREW_FLEX_PREFIX}/lib)
+    endif()
+endif()
+
+find_package(FLEX REQUIRED)
